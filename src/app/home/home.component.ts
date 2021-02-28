ce3a26e9e14a898e0a03d0b54318daabd13f5b82
shravani <shravani.manduva@blackswan-technologies.com>
2021-02-28 17:37:55 +0530

    mac-screen adjust

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,34 +9,34 @@ import { cardList } from '../core/common.constant';
 })
 export class HomeComponent implements OnInit {
   featuresList = cardList;
-  minIncome =100000;
-  maxIncome =300000;
+  minIncome = 100000;
+  maxIncome = 300000;
   incomeSliderVal = 100000;
   minExpense = 0;
-  maxExpense =300000;
+  maxExpense = 300000;
   expenseSliderVal = 0
-  selectedMonth =3;
-  monthsList: number[] = [3,6,9,12,18,24,30,36];
-  emiVal;
-  existingLoan:false;
-  remaningVal;
-  disable:boolean = true;
-  constructor() { 
+  selectedMonth = 3;
+  monthsList: number[] = [3, 6, 9, 12, 18, 24, 30, 36];
+  emiVal = 0;
+  existingLoan: false;
+  remaningVal = this.incomeSliderVal;
+  disable: boolean = true;
+  constructor() {
   }
 
   ngOnInit(): void {
   }
 
-    /**To calculate EMI
-     * @author : shravani
-     * @date : 25-feb-2021
-    */
-  calculate(){
+  /**To calculate EMI
+   * @author : shravani
+   * @date : 25-feb-2021
+  */
+  calculate() {
     this.remaningVal = this.incomeSliderVal - this.expenseSliderVal;
-    this.emiVal = (this.remaningVal / this.selectedMonth).toFixed(0);
-    if(this.incomeSliderVal > this.minIncome){
-    this.disable = false;
-    }else{
+    this.emiVal = Number((this.remaningVal / this.selectedMonth).toFixed(0));
+    if (this.incomeSliderVal > this.minIncome) {
+      this.disable = false;
+    } else {
       this.disable = true;
     }
   }
